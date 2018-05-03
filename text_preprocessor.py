2b2fecac4f4171854e8a7b64993c090cc47083e3
Leo Bitz <amanyamy@gmail.com>
2018-05-03 09:31:34 +0400

    fixed slight issues

--- text_preprocessor.py
+++ text_preprocessor.py
@@ -13,9 +13,9 @@ class TextPreProcessor:
         self.int2char = {}
         self.batch_size = batch_size
         self.seuqnce_length = seuqnce_length
-        self.load_charset()
         self.n_consonants = 0
         self.n_vowels = 0
+        self.load_charset()
 
     def load_charset(self):
         data = open(self.charset_file, encoding='utf-8').readlines()
@@ -33,7 +33,7 @@ class TextPreProcessor:
         self.int2char = int2char
         self.char2int = char2int
         self.char2tup = char2tup
-        self.n_consonants = len(char2tup)
+        self.n_consonants = len(data)
         self.n_vowels = 10
 
     def encode_text_to_num(self, text):
@@ -43,7 +43,7 @@ class TextPreProcessor:
 
     def encode_char(self, char):
         class_code, vowel_code = self.char2tup[char]
-        class_hot = one_hot_encode(class_code // 10, self.n_consonants)
+        class_hot = one_hot_encode(class_code, self.n_consonants)
         vowel_hot = one_hot_encode(vowel_code, self.n_vowels)
         return class_hot, vowel_hot
 
@@ -57,18 +57,19 @@ class TextPreProcessor:
         prev_left = tex_data_file.read(seq_length)
         data_file = h5py.File(filename, "a")
 
+        chunk_size = 40 * seq_length * batch_size
         train_X = data_file.create_dataset(
             'train_x', (0, seq_length, 1),
             maxshape=(None, seq_length, 1),
-            chunks=(1000, seq_length, 1))
+            chunks=(chunk_size, seq_length, 1))
         train_y = data_file.create_dataset(
             'train_y', (0, self.n_consonants),
             maxshape=(None,  self.n_consonants),
-            chunks=(1000,  self.n_consonants))
+            chunks=(chunk_size,  self.n_consonants))
         train_z = data_file.create_dataset(
             'train_z', (0, self.n_vowels),
             maxshape=(None, self.n_vowels),
-            chunks=(1000, self.n_vowels))
+            chunks=(chunk_size, self.n_vowels))
 
         n_rows = 0
         n_chars = len(prev_left)
@@ -117,5 +118,5 @@ h5_file_path = os.path.join(cwd, "data/data.h5")
 text_file_path = os.path.join(cwd, "data/big.txt")
 charset_file = os.path.join(cwd, "data/charset.txt")
 
-processor = TextPreProcessor(text_file_path, charset_file, 100, 100)
+processor = TextPreProcessor(text_file_path, charset_file, 128, 100)
 processor.text_to_bin(h5_file_path)
